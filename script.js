31748d895bd26e2cc6956dd00a1d01d34b347f6a
realmbs <119774290+realmbs@users.noreply.github.com>
2024-02-20 17:21:52 -0600

    playing w dynamic sizing

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ const createGridItems = (l, h = 1) => {
   for (let i = 0; i < (l * h); i++) {
     const newDiv = document.createElement('div');
     newDiv.classList.add('grid-item');
+    newDiv.style.width = `${100 / l}px`;
+    newDiv.style.height = `${100 / h}px`;
+    newDiv.style.flex = `0 0 ${100 / l}%`;
     container.appendChild(newDiv);
   }
 }
 
-createGridItems(16);
+createGridItems(16, 16);
