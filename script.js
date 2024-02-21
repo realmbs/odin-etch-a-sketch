288b3cc19ca37ccd3dff031722485be83cbb3208
realmbs <119774290+realmbs@users.noreply.github.com>
2024-02-20 18:31:27 -0600

    added input functionality

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const container = document.querySelector('.container');
 const resetButton = document.querySelector('.reset--button');
 const startButton = document.querySelector('.start--button');
-const inputWidth = document.querySelector('.width--input');
-const inputHeight = document.querySelector('.height--input');
 
 // reset to initial conditions
 resetButton.addEventListener('click', () => {
   const gridItems = document.querySelectorAll('.grid-item');
+  const inputWidth = document.querySelector('.width--input');
+  const inputHeight = document.querySelector('.height--input');
   gridItems.forEach(i => {
     i.style.backgroundColor = 'rebeccapurple';
   });
@@ -14,10 +14,16 @@ resetButton.addEventListener('click', () => {
   inputHeight.value = 'Enter height';
 })
 
+// get width + height from input, call createGridItems, redirect if > 100
 startButton.addEventListener('click', () => {
-
+  const inputWidth = Number(prompt('Enter width'));
+  const inputHeight = Number(prompt('Enter height'));
+  if (inputWidth < 100 && inputHeight < 100) {
+    createGridItems(inputWidth, inputHeight);
+  } else if (inputWidth >= 100 || inputHeight >= 100) {
+    alert('Please enter a number less than 100');
+  }
 })
-
 
 addEventListener('mouseover', (e) => {
   if (e.target.classList.contains('grid-item')) {
@@ -25,15 +31,12 @@ addEventListener('mouseover', (e) => {
   }
 })
 
-const createGridItems = (l, h = 1) => {
-  for (let i = 0; i < (l * h); i++) {
+const createGridItems = (w, h = 1) => {
+  for (let i = 0; i < (w * h); i++) {
     const newDiv = document.createElement('div');
     newDiv.classList.add('grid-item');
-    newDiv.style.width = `${100 / l}px`;
+    newDiv.style.width = `${100 / w}px`;
     newDiv.style.height = `${100 / h}px`;
-    newDiv.style.flex = `0 0 ${100 / l}%`;
     container.appendChild(newDiv);
   }
 }
-
-createGridItems(16, 16);
